c518f4b053e879153773bae77dbc6f8f7da4516a
Jeremy Badger <jeremebadger@gmail.com>
2023-04-15 16:38:18 -0400

    Added Csvhelper to the project.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        // All transaction values should be positive. Withdraw of $25 inputs and records positive 25
+
+        // Checking
+        // Cannot allow to go below $0
+        // Savings
+        // Cannot allow to go below $0
+        // Deposits
+        // Ensure value is positive
+
+        // Withdrawals
+        // Ensure value is positive
     }
-  }
+
+    class Program
+    {
+        static void Menu()
+        {
+            // See list of transactions from Savings
+            // See list of transactions from Checking
+
+            // Make deposit transaction for savings
+            // Write all transactions to file
+
+            // Make deposit transaction for checking
+            // Write all transactions to file
+
+            // Make withdrawal transaction for savings
+            // Write all transactions to file
+
+            // Make withdrawal transaction for checking
+            // Write all transactions to file
+
+            // See the balance of savings and checking (together?)
+            // See the balance of savings 
+            // See the balance of checking
+
+            Console.WriteLine("This is the menu");
+
+        }
+        static void Main(string[] args)
+        {
+            // Load past transactions from a file
+
+
+            // Menu
+        }
+    }
 }
